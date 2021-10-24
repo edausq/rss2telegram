297f501647ac7142d2071ea0d1e26dfe9758bb9e
Edouard Dausque <git@edouard.dausque.net>
2021-10-24 21:23:44 +0200

    Add dockerfile + debug logs

--- rss2telegram.py
+++ rss2telegram.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """rss2telegram"""
 
+import sys
 import os
+import time
+
 import feedparser
 import boto3
 import telegram
@@ -21,12 +24,16 @@ def main():
         current_items = get_rss_entries(flux['fluxUrl'])
         previous_items = flux['fluxOldItems']
         new_items = set(current_items).difference(previous_items)
+        print(f"working on {flux['fluxUrl']}. Found {len(current_items)} items. {len(new_items)} are new.")
         items_sent = send_items_to_telegram(TELEGRAM_BOT, new_items, flux['telegramChats'])
         update_flux(DYNAMODB_TABLE, flux, items_sent)
 
 def get_rss_entries(url):
     """Returns items id (list) from rss flux url"""
     feed = feedparser.parse(url)
+    if feed.status != 200:
+        print(f"got http {feed.status} for {url}")
+        return []
     return [entry["id"] for entry in feed.get("entries")]
 
 def get_all_flux(table):
@@ -52,6 +59,7 @@ def send_items_to_telegram(bot, items, chats):
     items_sent = []
     for chat in chats:
         for item in items:
+            time.sleep(1.4)
             bot.send_message(str(chat), item)
             items_sent.append(item)
     return items_sent
@@ -61,3 +69,6 @@ def get_telegram_status(bot):
     updates = bot.get_updates()
     for update in updates:
         print(str(update.effective_chat.id) + ' ' + update.effective_message.text)
+
+if __name__ == '__main__':
+    sys.exit(main())
